80bab28331b64a16d78c70fd7688c8cf372259dc
Sahid Al Masud Aayan <aayan.webdeveloper@gmail.com>
2025-03-02 14:59:34 +0600

    view

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const router = require('./router');
 const app = express()
 app.use(express.json())
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }))
 var cookieParser = require('cookie-parser')
